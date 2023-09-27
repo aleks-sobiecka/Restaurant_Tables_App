9e075ff81d9c3d0dbe138c3230c470c86880c11f
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-09-27 22:46:02 +0200

    Add server

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Table from './components/pages/Table/Table';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTables } from './redux/tablesRedux';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
       <Container>
         <Header />
@@ -19,5 +26,4 @@ const App = () => {
       </Container>
   );
 };
-
 export default App;
