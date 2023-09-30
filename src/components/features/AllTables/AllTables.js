1dacc5815391f7612dcccad1661935d77da528a7
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-09-30 21:07:40 +0200

    Add function'REMOVE_TABLE'

--- src/components/features/AllTables/AllTables.js
+++ src/components/features/AllTables/AllTables.js
@@ -2,30 +2,38 @@ import { useSelector } from "react-redux";
 import { getAllTables } from "../../../redux/tablesRedux";
 import { Stack, Card, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { removeTableRequest } from "../../../redux/tablesRedux";
 
 const AllTables = () => {
 
     const tables = useSelector(getAllTables);
+
+    const dispatch = useDispatch();
+
+    const handleRemove = e => {
+        e.preventDefault();
+        dispatch(removeTableRequest(e.target.value));
+    }
 
     return ( 
         <section>{tables.map(table => (
             <div key={table.id} action={table.status}>
                 <Stack direction="horizontal" gap={3}>
                     <Card border="light">
-                        <Stack direction="horizontal" gap={3}>
+                        <Stack direction="horizontal" gap={3} className="mb-3">
                             <h3 className='mr-3'>Table {table.number}</h3>
                             <Card.Title className="my-auto">Status:</Card.Title>
                             <Card.Text className="my-auto">{table.status}</Card.Text>
                         </Stack>
                     </Card>
-                    <NavLink to={"/table/" + table.id} className="ms-auto">
+                    <Button variant="outline-danger" className="ms-auto" onClick={handleRemove} value={table.id}>Delete table</Button>
+                    <NavLink to={"/table/" + table.id}>
                         <Button variant="primary">Show more</Button>
                     </NavLink>
                 </Stack>
-                <hr />
             </div>
-        ))}
+            ))}
         </section>
     )
 }
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -8,11 +8,13 @@ export const getTableById = ({ tables }, tableId) => tables.find(table => table.
 const createActionName = actionName => `app/tables/${actionName}`;
 const UPDATE_TABLES = createActionName('UPTATE_TABLES');
 const ADD_TABLE = createActionName('ADD_TABLE');
+const REMOVE_TABLE = createActionName('REMOVE_TABLE');
 const EDIT_TABLE = createActionName('EDIT_TABLE');
 
 // action creators
 export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
 export const addTable = payload => ({ type: ADD_TABLE, payload });
+export const removeTable = payload => ({ type: REMOVE_TABLE, payload });
 export const editTable = payload => ({ type: EDIT_TABLE, payload });
 export const fetchTables = () => {
   return (dispatch) => {
@@ -35,6 +37,21 @@ export const addTableRequest = (newTable) => {
       .then(() => dispatch(addTable(newTable)))
   }
 };
+export const removeTableRequest = (table) => {
+  console.log(table);
+  return (dispatch) => {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ table }),
+    };
+
+    fetch('http://localhost:3131/tables/' + table, options)
+    .then(() => dispatch(removeTable(table)))
+  }
+};
 
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
@@ -42,6 +59,8 @@ const tablesReducer = (statePart = [], action) => {
       return [...action.payload];
     case ADD_TABLE:
       return [...statePart, {...action.payload, id: shortid() }]
+    case REMOVE_TABLE:
+      return [...statePart.filter(table => table.id !== action.payload)];
     case EDIT_TABLE:
       return statePart.map(table => (table.id === action.payload.tableId ? { ...table, ...action.payload } : table)) ;
     default:
